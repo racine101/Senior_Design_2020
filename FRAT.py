00e932c963cb1c80bc4a47fd49c397c9badb8644
racine mclean <racine.mclean87@gmail.com>
2021-02-28 22:25:36 -0600

    made code more readable

--- FRAT.py
+++ FRAT.py
@@ -4,7 +4,7 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-# from PIL import ImageGrab
+
  
 path = 'C:/VS_Code/Senior_Design/testImages'
 images = []
@@ -15,7 +15,6 @@ for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
-print(classNames)
  
 def findEncodings(images):
     encodeList = []
@@ -23,7 +22,6 @@ def findEncodings(images):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
-        print(type(encode))
     return encodeList
     
 def markAttendance(name):
@@ -70,7 +68,6 @@ cap = cv2.VideoCapture(0)
  
 while True:
     success, img = cap.read()
-    #img = captureScreen()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
  
@@ -80,12 +77,10 @@ while True:
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        #print(faceDis)
         matchIndex = np.argmin(faceDis)
     
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            #print(name)
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
@@ -93,7 +88,6 @@ while True:
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
  
-    cv2.imshow('Webcam',img)
+    cv2.imshow('F.R.A.T',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-            #save_known_faces()
             break
